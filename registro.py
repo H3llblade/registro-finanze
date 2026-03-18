1f0b1f30dc573b4db4862ea413c8a66d81a58641
Pippopush <106027196+H3llblade@users.noreply.github.com>
2026-03-18 19:01:19 +0100

    Update registro.py

--- registro.py
+++ registro.py
@@ -165,10 +165,10 @@ else:
     st.info("Nessun movimento registrato")
 
 # ===============================
-# PULSANTE RESET REGISTRO
+# PULSANTE RESET
 # ===============================
-# st.divider()
-# st.subheader("⚠️ Gestione Registro")
-# if st.button("Svuota Registro"):
-#   st.session_state.dati = {"cassa": 0, "fondo_cassa": 0, "soldi_sporchi": 0, "movimenti": []}
-#   aggiorna_file_github(st.session_state.dati)
+st.divider()
+st.subheader("⚠️ Gestione Registro")
+if st.button("Svuota Registro"):
+st.session_state.dati = {"cassa": 0, "fondo_cassa": 0, "soldi_sporchi": 0, "movimenti": []}
+   aggiorna_file_github(st.session_state.dati)
